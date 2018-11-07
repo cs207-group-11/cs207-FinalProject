7b5cbaf0852d5885da750a4d2d22a83a2763efd7
Susana Xu <susanaxu@SusanadeMacBook-Pro.local>
2018-11-06 23:37:16 -0500

    fixed the unary problem

--- AutoDiff/AutoDiff.py
+++ AutoDiff/AutoDiff.py
@@ -7,7 +7,14 @@ else:
 	from . import BasicMath as bm
 
 class DualNumber:
+	""" This class defines dual number and the way of a series of arithmetic functions implemented on it."""
 	def __init__(self, val = None, der = None):
+		"""The constructor for DualNumber Class.
+
+		Args:
+			val (real number): The value of the function at a certain stage.
+			der (real number): The value of the derivate at a ceratin stage. If no value given for der in dual number, the default is 1.
+		"""
 		# This only applies for the case of scalar
 		if der is None:
 			self.val = val
@@ -17,12 +24,50 @@ class DualNumber:
 			self.der = der
 
 	def __add__(self, other):
+		"""Return the result of self + other as a dual number.
+
+		INPUTS
+			self (DualNumber object): The value of the function at a certain stage.
+			other (DualNumber object or real number): 
+				If it is dual number, then add its val and del respectively to the value and del of our recent dual number. 
+				If it is a real number, add it to the val of our recent dual number.
+
+		RETURNS
+			The result of self + other (DualNumber)
+
+		EXAMPLES
+		>>> ad = ad()
+		>>> x = 1	
+		>>> user_def = lambda x: x+2
+		>>> t = ad.auto_diff(function = user_def, eval_point = x)
+		>>> print(t.val, t.der)
+		3 1
+ 		"""        
 		if isinstance(other, DualNumber):
 			return DualNumber(self.val + other.val, self.der + other.der)
 		else:
 			return DualNumber(self.val + other, self.der)
 
 	def __radd__(self, other):
+		"""Return the result of other + self as a dual number use the __add__ above.
+
+		INPUTS
+			self (DualNumber object): The value of the function at a certain stage.
+			other (DualNumber object or real number): 
+				If it is dual number, then add its val and del respectively to the value and del of our recent dual number. 
+				If it is a real number, add it to the val of our recent dual number.
+
+		RETURNS
+			The result of self + other (DualNumber)
+
+		EXAMPLES
+		>>> ad = ad()
+		>>> x = 1	
+		>>> user_def = lambda x: x+2
+		>>> t = ad.auto_diff(function = user_def, eval_point = x)
+		>>> print(t.val, t.der)
+		3 1
+ 		"""      
 		# use __add__ defined above
 		return self + other
 
@@ -58,6 +103,9 @@ class DualNumber:
 
 	def __rpow__(self, other):
 		return DualNumber(other ** self.val, other ** self.val * bm.log(other) * self.der)
+
+	def __neg__(self):
+		return DualNumber(-self.val, -self.der)
 
 def log(x):
 	if isinstance(x, DualNumber):
@@ -135,5 +183,6 @@ if __name__ == '__main__':
 	t = ad.auto_diff(function = user_def, eval_point = x)
 	assert(t.val == 24.541133949029593)
 	assert(t.der == -249.5939593878782)
+	# assert(t.val == 0.2)
+	# assert(t.der == 1)
 
-# You can confirm the result using Wolfram Alpha!
