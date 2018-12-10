eed95210be097cc0a89f67911998d8fd144e6754
Zheyu Wu <zheyuwu@Zheyus-MacBook-Pro.local>
2018-12-09 20:47:28 -0500

    Implemented GUI to allow user input function and eval_point

--- Feature/Newton_fractal.py
+++ Feature/Newton_fractal.py
@@ -1,18 +1,23 @@
 import os
 import sys
+from tkinter import *
 from PIL import Image
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 
 module_path = os.path.join(os.path.abspath('../'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
 from VayDiff import VayDiff as AD
-from VayDiff import BasicMath as bm
-
+from VayDiff.BasicMath import *
 
 def color(ind, level):
 
@@ -107,8 +112,8 @@ def show_route(f, size, name, x_min=-2.0, x_max=2., y_min=-2.0, y_max=2.0, eps=1
     plt.ion()
     plt.imshow(img, extent=[-img.shape[1]/2., img.shape[1]/2., -img.shape[0]/2., img.shape[0]/2. ])
     print (img)
-    plt.xlim(-500,500)
-    plt.ylim(-500,500)
+    plt.xlim(-size/2,size/2)
+    plt.ylim(-size/2,size/2)
     ax = plt.axes()
     for x in tqdm(range(size)):
         for y in tqdm(range(size)):
@@ -123,17 +128,78 @@ def show_route(f, size, name, x_min=-2.0, x_max=2., y_min=-2.0, y_max=2.0, eps=1
                 c2 = c - f(c) / (ad.auto_diff(function = f, eval_point = [temp])).der['x']
 
             # Of course we will not draw all arrows for all points
-                if (x % 100 == 0 and y % 100 == 0):
+                if (x % (size//10) == 0 and y % (size//10) == 0):
                     if (abs(c2.real-c.real) > 0.1 or abs(c2.imag - c.imag) > 0.1):
-                        ax.arrow(c.real * 250, c.imag * 250, (c2.real-c.real) * 250, (c2.imag - c.imag) * 250, color = (177/255, 196/255, 226/255, 0.3), head_width= 10, head_length=10)
+                        ax.arrow(c.real * size / 4, c.imag * size / 4, (c2.real-c.real) * size / 4, (c2.imag - c.imag) * size / 4, color = (177/255, 196/255, 226/255, 0.3), head_width= size // 100, head_length = size // 100)
                         plt.pause(0.01)
                 if abs(c2 - c) < eps:
                     break
                 c = c2
 
+def launcn_UI():
+
+    root = Tk()
+    root.title("Newton's Fractal Generator")
+
+    derivative_order = StringVar(root)
+    derivative_order.set("1") # default value
+    drop_down_menu = OptionMenu(root, derivative_order, "1", "2")
+
+    #Labels
+    label_1 = Label(root, text = "Input Equation")
+    label_2 = Label(root, text = "Evaluation Point")
+    label_3 = Label(root, text = "Image Size")
+    label_4 = Label(root, text = "Derivative Order")
+
+    #Entries
+    entry_1 = Entry(root)
+    entry_2 = Entry(root)
+    entry_3 = Entry(root)
+    entry_3.insert(END, '500')
+
+
+    #Button
+    button_1 = Button(root, text = "Run", command = lambda: run_command(entry_1, entry_2, entry_3, derivative_order))
+    button_2 = Button(root, text = "Show Gradient", command = lambda: gradient_command(entry_1, entry_2, entry_3, derivative_order))
+
+    #Placements
+    label_1.grid(row = 1)
+    label_2.grid(row = 2)
+    label_3.grid(row = 3)
+    label_4.grid(row = 4)
+
+
+    entry_1.grid(row = 1, column = 1)
+    entry_2.grid(row = 2, column = 1)
+    entry_3.grid(row = 3, column = 1)
+    drop_down_menu.grid(row = 4, column = 1)
+    button_1.grid(row = 5, column = 0)
+    button_2.grid(row = 5, column = 1)
+
+
+    root.mainloop()
+
+def run_command(entry_1, entry_2, entry_3, derivative_order):
+    equation = entry_1.get()
+    eval_point = float(entry_2.get())
+    img_size = int(entry_3.get())
+    deri_order = int(derivative_order.get())
+
+    g = lambda x: eval(equation)
+    draw(g, img_size, "old.png")
+
+
+def run_command(entry_1, entry_2, entry_3, derivative_order):
+    equation = entry_1.get()
+    eval_point = float(entry_2.get())
+    img_size = int(entry_3.get())
+    deri_order = int(derivative_order.get())
+
+    g = lambda x: eval(equation)
+    show_route(g, img_size, "old.png")
+
 def f(x):
     return x**6-1
 
 if __name__ == '__main__':
-    draw(f, 1000, "old.png")
-    show_route(f, 1000, "old.png")
+    launcn_UI()
